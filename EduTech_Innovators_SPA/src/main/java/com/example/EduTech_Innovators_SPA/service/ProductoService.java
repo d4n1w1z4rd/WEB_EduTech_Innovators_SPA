8e6656af9e4b2efab455003b270e7dc583c9770b
esbenavides19 <131211143+esbenavides19@users.noreply.github.com>
2025-05-20 14:11:07 -0400

    Update ProductoService.java
    agregamos modelo de Producto, Repositorio y continuamos con el Service del producto

--- EduTech_Innovators_SPA/src/main/java/com/example/EduTech_Innovators_SPA/service/ProductoService.java
+++ EduTech_Innovators_SPA/src/main/java/com/example/EduTech_Innovators_SPA/service/ProductoService.java
@@ -1,5 +1,15 @@
 package com.example.EduTech_Innovators_SPA.service;
 
+import com.example.EduTech_Innovators_SPA.model.Producto;
+import com.example.EduTech_Innovators_SPA.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class ProductoService {
+    @Autowired
+    private ProductoRepository productoRepository;
+
     
 }
